adba668d5f4a96a195471080afb79e03787914f9
rhcode <rhonwade@cmu.edu>
2015-03-04 12:02:30 -0800

    Testing jenkins auto build

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -10,8 +10,8 @@ public class Main
 {
 	final static Logger logger = Logger.getLogger(Main.class);
 	
-    public static void main( String[] args )
-    {
-        logger.debug( "Hello World!" );
+    public static void main(String[] args) {
+        logger.debug("Starting calculator");
+        logger.debug("Testing jenkins auto build");
     }
 }
