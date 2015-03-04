b0983010fa339e760a3751b5183eb65c4164e8f5
rhcode <rhonwade@cmu.edu>
2015-03-04 12:07:37 -0800

    Try 2

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -12,6 +12,6 @@ public class Main
 	
     public static void main(String[] args) {
         logger.debug("Starting calculator");
-        logger.debug("Testing jenkins auto build");
+        logger.debug("Testing jenkins auto build #2");
     }
 }
