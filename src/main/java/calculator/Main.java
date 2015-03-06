c9f4044ae904134e92943c0f11fc20a4927f1157
rhcode <rhonwade@cmu.edu>
2015-03-06 11:14:29 -0800

    Provided description and null sanity check

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -3,15 +3,22 @@ package calculator;
 import org.apache.log4j.Logger;
 
 /**
- * Hello world!
- *
+ * This the entry-point class for the calculator. 
+ * Orchestrates calls to the Solver class to solve 
+ * the input string
  */
 public class Main 
 {
 	final static Logger logger = Logger.getLogger(Main.class);
 	
     public static void main(String[] args) {
-        logger.debug("Starting calculator");
-        logger.debug("Testing jenkins auto build #4");
+        logger.info("Starting calculator");
+        if (args[0] == null || args[0].trim().length() == 0) {
+        	logger.error("No input provided");
+        	return;
+        }
+        
+        logger.debug("The input received is : " + args[0]);
+        
     }
 }
