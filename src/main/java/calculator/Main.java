12e49dc77a8fa6270d298b91bc6eb5b864a76a62
rhcode <rhonwade@cmu.edu>
2015-03-06 20:28:45 -0800

    Added sanity check for input

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -13,12 +13,44 @@ public class Main
 	
     public static void main(String[] args) {
         logger.info("Starting calculator");
-        if (args[0] == null || args[0].trim().length() == 0) {
-        	logger.error("No input provided");
-        	return;
-        }
-        
         logger.debug("The input received is : " + args[0]);
+        boolean isInputCorrect = sanityCheck(args[0]);
+        if (isInputCorrect == false)
+        	return;
         
+        
+    }
+    
+    private static boolean sanityCheck(String input) {
+    	if (input == null || input.trim().length() == 0) {
+        	logger.error("No input provided");
+        	return false;
+        }
+    	
+    	char curChar, prevChar = 'x';
+    	int bracesCount = 0;
+    	for (int i = 0; i < input.length(); i++) {
+    		curChar = input.charAt(i);
+    		switch(curChar) {
+    			case '(' : bracesCount++;
+    					   break;
+    					   
+    			case ')' : bracesCount--;
+    					   if (bracesCount < 0) {
+    						   logger.error("Input is not well formatted. Check braces");
+    						   return false;
+    					   }
+    					   break;
+    		
+    			case ',' : if (i > 0 && prevChar == ',') {
+    							logger.error("Extra commas present in input");
+    							return false;
+    					   }
+    			
+    			default : break;
+    		}
+    		prevChar = curChar;
+    	}
+    	return true;
     }
 }
