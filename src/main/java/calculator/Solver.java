9ae219fb8980c075ed4c765e7c2093323414c4d6
rhcode <rhonwade@cmu.edu>
2015-03-06 23:37:47 -0800

    Added code to push all operators and operands on stack

--- src/main/java/calculator/Solver.java
+++ src/main/java/calculator/Solver.java
@@ -1,7 +1,7 @@
 package calculator;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 import org.apache.log4j.Logger;
@@ -13,11 +13,8 @@ import org.apache.log4j.Logger;
 public class Solver {
 	final static Logger logger = Logger.getLogger(Solver.class);
 	
-    //Tracks the index in the input from which processing still needs to be done 
-	private int startIndex;
-	
-	//Set of all valid operations that can be performed TODO : let command
-	private Set<String> operations = null;
+	//Map of all valid operations that can be performed TODO : let command
+	private Map<String, String> operations = null;
 	
 	//Stack to save all the operations and operands
 	private Stack<String> tokenStack = null;
@@ -28,30 +25,48 @@ public class Solver {
 	private int bracesCount = 0;
 	
 	public Solver() {
-		startIndex = 0;
-		
-		operations = new HashSet<String>();
-		operations.add("add");
-		operations.add("sub");
-		operations.add("mult");
-		operations.add("div");
+		operations = new HashMap<String, String>();
+		operations.put("add","+");
+		operations.put("sub","-");
+		operations.put("mult","*");
+		operations.put("div","/");
 		
 		tokenStack = new Stack<String>();
 		tempStack = new Stack<Integer>();
 	}
 	
-	//TODO : Need to complete
-	public int solve (String input) {
-		for (int i = 0; i < input.length(); i++) {
+	
+	public void populateOperandAndOperatorStack (String input) {
+		for (int i = 0; i < input.length();) {
 			char curChar = input.charAt(i);
-			if (Character.isLetterOrDigit(curChar)) {
-				String command = input.substring(startIndex, i+1);
-				if (operations.contains(command)) {
-				
-				}
-			
+			if (Character.isAlphabetic(curChar)) {
+				String command = getCommand(input.substring(i));
+				i += command.length();
+				tokenStack.add(operations.get(command));
 			}
+			else if (Character.isDigit(curChar)) {
+				String number = getNumber(input.substring(i));
+				i += number.length();
+				tokenStack.add(number);
+			}
+			else 
+				i++;
 		}
-		return 1;
+	}
+	
+	private String getCommand(String substr) {
+		int indexOfOpeningBrace = substr.indexOf("(");
+		return substr.substring(0, indexOfOpeningBrace);
+	}
+	
+	private String getNumber(String substr) {
+		int indexOfComma = substr.indexOf(",");
+		int indexOfClosingBrace = substr.indexOf(")");
+		int lesserValueIndex = 0;
+		if (indexOfComma > 0)
+			lesserValueIndex = Math.min(indexOfComma, indexOfClosingBrace);
+		else
+			lesserValueIndex = indexOfClosingBrace;
+		return substr.substring(0, lesserValueIndex);
 	}
 }
